db13246ecc34ba9e734f6f5a018e9ffa51144bd5
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-11-09 09:19:28 +0700

    SocialAPI
    Spring Boot – Spring Data JPA

---
+++ SocialAPI/src/main/java/com/hiro11/SocialAPI/student/StudentController.java
@@ -0,0 +1,8 @@
+package com.hiro11.SocialAPI.student;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+}
---
+++ SocialAPI/src/main/java/com/hiro11/SocialAPI/student/StudentService.java
@@ -0,0 +1,8 @@
+package com.hiro11.SocialAPI.student;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+}
